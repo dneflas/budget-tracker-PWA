6a5f365ec56b6eba4f5023e1c5d8c7fd4190d014
Debbie Neflas <debbieneflas@gmail.com>
2022-07-06 16:59:00 -0700

    fix bug in files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["./index.html", "./css/style.css", "./js/index.js"];
+const FILES_TO_CACHE = ["./index.html", "./css/styles.css", "./js/index.js"];
 const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
