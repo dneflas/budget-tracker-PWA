92150a8be9d748bb5144f14798807a65fecbb16c
Debbie Neflas <debbieneflas@gmail.com>
2022-07-06 16:52:42 -0700

    add files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["./index.html"];
+const FILES_TO_CACHE = ["./index.html", "./css/style.css", "./js/index.js"];
 const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
